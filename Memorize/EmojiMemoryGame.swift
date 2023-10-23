6fc7e2f5f10b20eba1912ca24dc1cb9a153ac923
v-engr <v-engr@ya.ru>
2023-10-23 21:14:59 +0300

    Lecture 6 finished

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -11,7 +11,7 @@ class EmojiMemoryGame: ObservableObject {
     private static let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "😱", "👹", "☠️", "👺", "👽", "🤖"]
     
     private static func createMemoryGame() -> MemoryGame<String> {
-        return MemoryGame(numberOfPairsOfCards: 4) { pairIndex in
+        return MemoryGame(numberOfPairsOfCards: 12) { pairIndex in
             if emojis.indices.contains(pairIndex) {
                 return emojis[pairIndex]
             } else {
@@ -21,9 +21,7 @@ class EmojiMemoryGame: ObservableObject {
     }
     
     @Published
-    private var model = MemoryGame(numberOfPairsOfCards: 10) { pairIndex in
-        return emojis[pairIndex]
-    }
+    private var model = createMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
